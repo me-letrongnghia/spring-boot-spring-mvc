543b8d6af324609948f30859873ece5a25d2d22e
letrongnghia <nghialee2301@gmail.com>
2024-01-12 23:03:45 +0700

    Using @RequestParam Annotation

--- 01-thymeleafdemo-helloworld/src/main/java/com/nghiale/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 01-thymeleafdemo-helloworld/src/main/java/com/nghiale/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Locale;
 
@@ -35,6 +36,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionThree")
+    public String letsShoutDudev3(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
